7d1e1ceb1e1cefa06c9a7df4b4fc7e1c8e63f905
red-x-silver <milkjazzy@gmail.com>
2024-08-18 12:17:24 +0100

    [xy] added paths file

---
+++ paths.py
@@ -0,0 +1,19 @@
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(__file__, '../../'))
+
+DATA_DIR = os.path.join(ROOT_DIR, "freesound-oneshots-dataset")
+OUT_DIR = os.path.join(ROOT_DIR, "test-output")
+
+KK_DIR = os.path.join(DATA_DIR, "kick_samples")
+SN_DIR = os.path.join(DATA_DIR, "snare_samples")
+HH_DIR = os.path.join(DATA_DIR, "hh_samples")
+
+assert os.path.isdir(DATA_DIR)
+assert os.path.isdir(KK_DIR)
+assert os.path.isdir(SN_DIR)
+assert os.path.isdir(HH_DIR)
+assert os.path.isdir(OUT_DIR)
+
+
+#from paths import KK_DIR, SN_DIR, HH_DIR, OUT_DIR
